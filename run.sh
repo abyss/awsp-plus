a861b8f352cd26ff37692fba31fc9fbe1f916e24
Abyss <abyss@users.noreply.github.com>
2021-10-03 20:42:27 -0400

    feat: allow typed profile to bypass selector

--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-AWS_PROFILE="$AWS_PROFILE" _awsp_prompt
+if [ $# -eq 0 ]; then
+  AWS_PROFILE="$AWS_PROFILE" _awsp_prompt
+  selected_profile="$(cat $HOME/.awsp)"
+else
+  selected_profile="$@"
+  echo "$selected_profile" > $HOME/.awsp
+fi
 
-selected_profile="$(cat ~/.awsp)"
-
-if [ -z "$selected_profile" ]
-then
+if [ -z "$selected_profile" ] || [ "$selected_profile" == "default" ]; then
   unset AWS_PROFILE
 else
   export AWS_PROFILE="$selected_profile"
